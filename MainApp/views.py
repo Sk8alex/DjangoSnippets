5f5e14ed42154b924340c3fd050a707f44c458f3
Martynov A.A <sk8alex@mail.ru>
2025-08-03 11:10:58 +0300

    Add error for id not found

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def index_page(request):
@@ -22,15 +23,14 @@ def snippets_page(request):
     return render(request, 'pages/view_snippets.html', context)
 
 
-def snippet_info(request, snippet_id: int):
+def snippet_detail(request, snippet_id: int):
+    context = {'pagename': 'Просмотр сниппета'}
     try:
         snippet = Snippet.objects.get(id=snippet_id)
-    except Snippet.DoesNotExist:
-        raise Http404(f"Сниппет с id={snippet_id} не найден")
+    except ObjectDoesNotExist:
+        return render(request, 'pages/errors.html', context | {"error": f"Сниппет с id={snippet_id} не найден"})
+
     else:
-        context = {
-            'pagename': 'Сниппет',
-            'snippet': snippet,
-        }
-        return render(request, 'pages/snippet_info.html', context)
+        context['snippet'] = snippet
+        return render(request, 'pages/snippet_detail.html', context)
 
