e36130bdee0d17ff1cde3b55d0807989b733bf2f
Martynov A.A <sk8alex@mail.ru>
2025-08-07 10:55:12 +0300

    Create new snippet in form and Add url for create snippet

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,4 +1,4 @@
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
 from django.core.exceptions import ObjectDoesNotExist
@@ -16,7 +16,6 @@ def add_snippet_page(request):
         'pagename': 'Добавление нового сниппета',
         'form': form
         }
-    
     return render(request, 'pages/add_snippet.html', context)
 
 
@@ -35,13 +34,17 @@ def snippet_detail(request, snippet_id: int):
         snippet = Snippet.objects.get(id=snippet_id)
     except ObjectDoesNotExist:
         return render(request, 'pages/errors.html', context | {"error": f"Сниппет с id={snippet_id} не найден"})
-
     else:
         context['snippet'] = snippet
         return render(request, 'pages/snippet_detail.html', context)
     
 
 def create_snippet(request):
-    print(f"{request.POST = }")
-    return HttpResponse("Ok")
+    if request.method == "POST":
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("snippets-list") #URL для списка сниппетов
+        return render(request, "pages/add_snippet.html", context={"form": form})
+    return HttpResponseNotAllowed(["POST"], "You must make POST request to add snippet.")
 
