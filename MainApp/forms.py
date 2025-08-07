dbfaec50045185034e36b13e7b12e81804bee5d3
Martynov A.A <sk8alex@mail.ru>
2025-08-07 11:50:46 +0300

    Change default options for SnippetForm

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -1,6 +1,30 @@
-from django.forms import ModelForm, ValidationError
+from django.forms import ModelForm, ValidationError, Textarea, TextInput
 from MainApp.models import Snippet
 #rom django.forms import Form
+
+
+# Описание возможностей по настройке форм
+# https://docs.djangoproject.com/en/dev/ref/forms/widgets/#django.forms.Widget.attrs
+
+class SnippetForm(ModelForm):
+    class Meta:
+        model = Snippet
+        # Описываем поля, которые будем заполнять в форме
+        fields = ['name', 'lang', 'code']
+        labels = {"name": "", "lang": "", "code": ""}
+        widgets = {
+            "name": TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Название сниппета",
+                "style": "max-width: 300px"
+            }),
+            "code": Textarea(attrs={
+                "placeholder": "Код сниппета",
+                "rows": 5,
+                "class": "input-large",
+                "style": "width: 50% !important; resize: vertical !important;"
+            }),  
+        }
 
 
 
