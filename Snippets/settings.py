9b1265a3114eb482870ebbf3d644ba577eade87d
Martynov A.A <sk8alex@mail.ru>
2025-08-14 12:35:38 +0300

    ALLOWED_HOSTS edit

--- Snippets/settings.py
+++ Snippets/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = get_random_secret_key()
 #DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
